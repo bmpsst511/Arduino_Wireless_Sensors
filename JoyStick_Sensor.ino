3ecf694400e0302a795d27bf3521138410c8981c
Jen-Tun Lee <bmpsst511@gmail.com>
2020-02-09 20:29:24 +0900

    Add files via upload

---
+++ JoyStick_Sensor.ino
@@ -0,0 +1,15 @@
+void setup()
+{
+  Serial.begin(9600); //Set serial baud rate to 9600 bps
+}
+void loop()
+{
+int val;
+int val2;
+val=analogRead(0);//Read slider value from analog 0
+val2=analogRead(2);//Read slider value from analog 0
+//Serial.print(val,DEC);//Print the value to serial port
+//Serial.println(val2,DEC);//Print the value to serial port
+Serial.println(val+val2,DEC);//Print the value to serial port
+delay(100);
+}
