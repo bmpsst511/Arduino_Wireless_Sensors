a5f84f032eeef5c71f16793c1b5882d446926c18
Jen-Tun Lee <bmpsst511@gmail.com>
2020-03-25 23:02:08 +0800

    Create BNO055.ino

---
+++ IMU BNO055/BNO055.ino
@@ -0,0 +1,100 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiUDP.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+String PoseX, PoseY, PoseZ, On;
+/* Set the delay between fresh samples */
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+/**************************************************************************/
+/*
+    Displays some basic information on this sensor from the unified
+    sensor API sensor_t type (see Adafruit_Sensor for more information)
+*/
+/**************************************************************************/
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  bno.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" xxx");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" xxx");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx"); 
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+ 
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Orientation Sensor Test"); Serial.println("");
+  pinMode(TouchSensor, INPUT);
+
+   /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
+    /* Use external crystal for better accuracy */
+  bno.setExtCrystalUse(true);
+   
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+  
+}
+ 
+void loop() {
+
+/* Get a new sensor event */
+  sensors_event_t event;
+  bno.getEvent(&event);
+  imu::Vector<3> linAcc = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+  /* Board layout:
+         +----------+
+         |         *| RST   PITCH  ROLL  HEADING
+     ADR |*        *| SCL
+     INT |*        *| SDA     ^            /->
+     PS1 |*        *| GND     |            |
+     PS0 |*        *| 3VO     Y    Z-->    \-X
+         |         *| VIN
+         +----------+
+  */
+
+
+  /* The processing sketch expects data as roll, pitch, heading */
+  //Serial.print(F("Orientation: "));
+  Serial.print((float)event.orientation.x);Serial.print(";");
+ // Serial.print(F(" "));
+  Serial.print((float)event.orientation.y);Serial.print(";");
+ // Serial.print(F(" "));
+  Serial.print((float)event.orientation.z);Serial.print("\t");
+ // Serial.println(F(""));
+  PoseX = (float)event.orientation.x;
+    PoseY = (float)event.orientation.y;
+      PoseZ = (float)event.orientation.z;
+  /* Also send calibration data for each sensor. */
+  uint8_t sys, gyro, accel, mag = 3;
+  bno.getCalibration(&sys, &gyro, &accel, &mag);
+ /* Serial.print(F("Calibration: "));
+  Serial.print(sys, DEC);
+  Serial.print(F(" "));
+  Serial.print(gyro, DEC);
+  Serial.print(F(" "));
+  Serial.print(accel, DEC);
+  Serial.print(F(" "));
+  Serial.println(mag, DEC);*/
+  Serial.print("\r\n");
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+  }
