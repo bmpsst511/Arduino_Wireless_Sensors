c4ca18a10fcbd36fe433e90e0e6aafe8a822b6be
Jen-Tun Lee <bmpsst511@gmail.com>
2023-02-26 00:11:40 +0800

    led flash to indicate the connection state of WIFI

--- PulseSensors/GrrovePulseSensor.cpp
+++ PulseSensors/GrrovePulseSensor.cpp
@@ -8,7 +8,7 @@
 const char *ssid =  "xxx";     // replace with your wifi ssid and wpa2 key
 const char *pass =  "xxx";
 
-const IPAddress outIp(172,20,10,5); //分享器給你Server的IP位址
+const IPAddress outIp(172,20,10,7); //分享器給你Server的IP位址
 const unsigned int outPort = 8000;
 const unsigned int localPort = 6969;
 
@@ -20,52 +20,57 @@ WiFiUDP Udp;
  
 void setup() 
 {
-  Serial.begin(9600); /* begin serial for debug */
+    Serial.begin(9600); /* begin serial for debug */
+    pinMode(LED_BUILTIN, OUTPUT);
 
-  delay(10);
+    delay(10);
 
-  Serial.println("Connecting to ");
-  Serial.println(ssid); 
-  WiFi.begin(ssid, pass);
+    Serial.println("Connecting to ");
+    Serial.println(ssid); 
+    WiFi.begin(ssid, pass);
 
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected"); 
-  Serial.print("IP Address: "); 
-  Serial.println(WiFi.localIP());
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(500);
+        digitalWrite(LED_BUILTIN, LOW);
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println("");
+    Serial.println("WiFi connected"); 
+    Serial.print("IP Address: "); 
+    Serial.println(WiFi.localIP());
+    digitalWrite(LED_BUILTIN, LOW);
 
-  // Start the UDP client
-  Udp.begin(localPort);
-  Wire.begin(D1, D2); /* join i2c bus with SDA=D1 and SCL=D2 of NodeMCU */
+    // Start the UDP client
+    Udp.begin(localPort);
+    Wire.begin(D2, D1); /* join i2c bus with SDA=D2 and SCL=D1ß of NodeMCU */
 }
- 
+
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
 
-  /**開始發送資料給Server端 **/
-  // Send the distance to the client, along with a break to separate our messages
+    /**開始發送資料給Server端 **/
+    // Send the distance to the client, along with a break to separate our messages
 
-  //Client.beginPacket(ip,27); //前面指定的Port
-  // heart beat
-  OSCMessage msg("/osc/inputs");
-  Wire.requestFrom(0xA0 >> 1, 1);    // request 1 bytes from slave device
-  while(Wire.available()) {          // slave may send less than requested
-    unsigned char c = Wire.read();   // receive heart rate value (a byte)
-    Serial.println(c, DEC);         // print heart rate value
-    //Serial.println(c);         // print heart rate value
-    //Serial.print(".");
-    //Start to send value to max/msp
-    //Udp.println(c, DEC);
-    msg.add(c);
-    Udp.beginPacket(outIp, outPort);
-    msg.send(Udp);
-    Udp.endPacket();
-  }
+    //Client.beginPacket(ip,27); //前面指定的Port
+    // heart beat
+    OSCMessage msg("/osc/inputs");
+    Wire.requestFrom(0xA0 >> 1, 1);    // request 1 bytes from slave device
+    while(Wire.available()) {          // slave may send less than requested
+        unsigned char c = Wire.read();   // receive heart rate value (a byte)
+        Serial.println(c, DEC);         // print heart rate value
+        //Serial.println(c);         // print heart rate value
+        //Serial.print(".");
+        //Start to send value to max/msp
+        //Udp.println(c, DEC);
+        msg.add(c);
+        Udp.beginPacket(outIp, outPort);
+        msg.send(Udp);
+        Udp.endPacket();
+    }
 
-  //delay(BNO055_SAMPLERATE_DELAY_MS);
-  delay(500);
+    //delay(BNO055_SAMPLERATE_DELAY_MS);
+    delay(500);
 }
